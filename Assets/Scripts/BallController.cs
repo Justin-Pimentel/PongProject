abafdba33652eb94275592437900789147c8cf70
Justin <jpimntel@gmail.com>
2020-05-09 12:06:19 -0700

    Project Update
    Added more functionality to the ball and the paddles

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public float initialSpeed;
+    private GameObject lP;
+    private GameObject rP;
+    private Vector2 newVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lP = GameObject.Find("LeftPaddle");
+        rP = GameObject.Find("RightPaddle");
+
+        float rNum = Random.Range(-1f, 1f);
+
+        if (rNum <= 0.0f)
+        {
+            newVelocity = new Vector2(-1f, 0f);
+        }else if(rNum > 0.0f)
+        {
+            newVelocity = new Vector2(1f, 0f);
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Move the ball
+        transform.Translate(newVelocity * initialSpeed);
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        //Get collision info
+        string pName = coll.gameObject.name;
+        string tag = coll.gameObject.tag;
+        ContactPoint2D p = coll.contacts[0];
+        float d = 0.0f;
+
+        //Vectors to calculate reflection angle off of paddles and walls
+        Vector2 inNormal = coll.contacts[0].normal;
+        newVelocity = Vector2.Reflect(newVelocity, inNormal);
+        Debug.Log("newVelocity: " + newVelocity);
+
+        /*
+         * If a paddle was hit, find the difference from contact point to paddle center
+         * We will use this to offset the angle of reflection so the farther from center
+         * the more extreme the angle of reflection
+         */
+        if (tag == "Paddle")
+        {
+            d = findDiff(pName, p);
+            Debug.Log(d);
+        }
+
+        //Use newVelocity to modify reflect angle
+    }
+
+    float findDiff(string n, ContactPoint2D p)
+    {
+        float diff = 0.0f;
+
+        //For left paddle
+        if (n == "LeftPaddle")
+        {
+            diff = p.point.y - lP.transform.position.y;
+        }
+
+        //For right paddle
+        if (n == "RightPaddle")
+        {
+            diff = p.point.y - rP.transform.position.y;
+        }
+
+        return diff;
+    }
+}
---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+    private GameObject lPaddle;
+    private GameObject rPaddle;
+    private Rigidbody2D lPaddleBody;
+    private Rigidbody2D rPaddleBody;
+    public float lVelocity;
+    public float rVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Find gameobjects from scene hierarchy to get an accurate object reference
+        lPaddle = GameObject.Find("LeftPaddle");
+        rPaddle = GameObject.Find("RightPaddle");
+
+        //Just in case, modify paddle positions to be centered on game start
+        lPaddle.transform.position = new Vector3(lPaddle.transform.position.x, 0.0f, lPaddle.transform.position.z);
+        rPaddle.transform.position = new Vector3(rPaddle.transform.position.x, 0.0f, rPaddle.transform.position.z);
+
+        //Get Rigidbody references for both paddles
+        lPaddleBody = lPaddle.GetComponent<Rigidbody2D>();
+        lPaddleBody.isKinematic = true;
+
+        rPaddleBody = rPaddle.GetComponent<Rigidbody2D>();
+        rPaddleBody.isKinematic = true;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            lPaddleBody.MovePosition(lPaddleBody.transform.position + transform.up * Time.fixedDeltaTime * lVelocity);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            lPaddleBody.MovePosition(lPaddleBody.transform.position + -(transform.up * Time.fixedDeltaTime * lVelocity));
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rPaddleBody.MovePosition(rPaddleBody.transform.position + transform.up * Time.fixedDeltaTime * rVelocity);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rPaddleBody.MovePosition(rPaddleBody.transform.position + -(transform.up * Time.fixedDeltaTime * rVelocity));
+        }
+    }
+}
