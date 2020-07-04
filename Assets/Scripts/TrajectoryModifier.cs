a064c4a8c143a01d95725b65a810dc758b1c1389
Justin-Pimentel <jfpiment@ucsc.edu>
2020-07-04 15:18:34 -0700

    Update TrajectoryModifier.cs

--- Assets/Scripts/TrajectoryModifier.cs
+++ Assets/Scripts/TrajectoryModifier.cs
@@ -1,18 +1,49 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[RequireComponent(typeof(LineRenderer))]
 
 public class TrajectoryModifier : MonoBehaviour
 {
+    public Vector3[] points;
+    public LineRenderer lR;
+    private int RESOLUTION = 50;
+    private int layerOrder = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(!lR)
+        {
+            lR = GetComponent<LineRenderer>();
+        }
+        lR.sortingLayerID = layerOrder;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    
+    //Helper function to visualize the curve
+    void DrawCurve()
+    {
+        for(int i = 0; i < RESOLUTION; i++)
+        {
+            float timestep = i/(float)RESOLUTION;
+            Vector3 linePoint = CalculateBezierPoint(timestep, points[1], points[2], points[3]);
+        }
+    }
+    
+    Vector3 CalculateBezierPoint(float t, Vector3 p1, Vector3 p2, Vector3 p3)
+    {
+        float u = 1-t;
+        float uu = u*u;
+        float tt = t*t;
+        
+        Vector3 point = (uu*p1)+(2*u*t*p2)+(tt*p3);
+        
+        return point;
     }
 }
